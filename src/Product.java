f51b53115b6648f1573ac3c37337e0f406c9d259
BT_1E10_28 <jeffreyc115@nycstudents.net>
2022-02-11 10:42:37 -0500

    Lab

--- src/Product.java
+++ src/Product.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+
 public class Product
 {
     private String name;
     private int quantity;
+    private double weight;
 
-    public Product(String name, int quantity)
+    public Product(String name, int quantity,double weight)
     {
         this.name = name;
         this.quantity = quantity;
+        this.weight=weight;
     }
 
     /** Returns the name of the product */
@@ -20,5 +24,10 @@ public class Product
     public int getQuantity()
     {
         return quantity;
+    }
+
+    public double getWeight()
+    {
+        return weight;
     }
 }
